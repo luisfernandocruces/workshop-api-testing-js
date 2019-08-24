d4b4d159062b9cb37f8e42e06158db7ebe23a50c
LuisCruces <luisfcv97@gmail.com>
2019-08-24 11:29:15 -0500

    integration with travis

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -1,11 +1,9 @@
 const agent = require('superagent');
 const statusCode = require('http-status-codes');
 const chai = require('chai');
-
 const expect = chai.expect;
 
 describe('First Api Tests', () => {
-
 
     it('Consume GET Service', async () => {
         const response = await agent.get('https://httpbin.org/ip');
@@ -13,23 +11,25 @@ describe('First Api Tests', () => {
         expect(response.body).to.have.property('origin');
     });
 
+
     it('Consume GET Service with query parameters', async () => {
         const query = {
         name: 'John',
         age: '31',
         city: 'New York'
         };
-    
         const response = await agent.get('https://httpbin.org/get').query(query);   
         expect(response.status).to.equal(statusCode.OK);
         expect(response.body.args).to.eql(query);
 
     });
 
+
     it('Consume HEAD Service', async () => {
         const response = await agent.head('https://httpbin.org/ip');
         expect(response.status).to.equal(statusCode.OK);
     });
+
 
     it('Consume PATCH Service', async () => {
         const query = {
@@ -37,11 +37,10 @@ describe('First Api Tests', () => {
             age: '31',
             city: 'New York'
         };
-
         const response = await agent.patch('https://httpbin.org/patch').query(query);
-    
         expect(response.status).to.equal(statusCode.OK);
     });
+
 
     it('Consume PUT Service', async () => {
         const query = {
@@ -49,11 +48,10 @@ describe('First Api Tests', () => {
             age: '31',
             city: 'New York'
         };
-
         const response = await agent.put('https://httpbin.org/put').query(query);
-    
         expect(response.status).to.equal(statusCode.OK);
     });
+
 
     it('Consume DELETE Service', async () => {
         const query = {
@@ -61,10 +59,7 @@ describe('First Api Tests', () => {
             age: '31',
             city: 'New York'
         };
-
         const response = await agent.delete('https://httpbin.org/delete').query(query);
-    
         expect(response.status).to.equal(statusCode.OK);
     });
-
 });
